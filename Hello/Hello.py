e141e779c76a21be9594a42790dc01752134678c
unknown <hemanthchallacs@gmail.com>
2020-12-10 02:26:34 +0530

    HelloWorld

---
+++ Hello/Hello.py
@@ -0,0 +1,10 @@
+print("Hello")
+print("*"*20)
+pounds = input("Enter pounds ")
+print('No of pounds ' +pounds)
+pounds = int(pounds)
+pounds = pounds*0.45259
+print('No of kilograms = '+ str(pounds))
+
+#2 hour mark in video 
+
